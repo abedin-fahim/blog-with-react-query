43eddef9d06a998a9b62ef9558d62fc71d03bfc5
Joynal Abedin <cfcfahim26@gmail.com>
2023-09-23 11:52:34 +0600

    optimistic update

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -3,7 +3,7 @@ import { useQuery, useMutation } from '@tanstack/react-query';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
-import { fetchEvent, updateEvent } from '../../utils/http.js';
+import { fetchEvent, updateEvent, queryClient } from '../../utils/http.js';
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 
@@ -20,6 +20,25 @@ export default function EditEvent() {
 
   const { mutate } = useMutation({
     mutationFn: updateEvent,
+    // Optimistic updating
+    // We can update the cache before the mutation is completed
+    // This is useful for a better user experience
+    onMutate: async (data) => {
+      const newEvent = data.event;
+
+      await queryClient.cancelQueries({ queryKey: ['events', { id: id }] });
+      const previousEvent = queryClient.getQueryData(['events', { id: id }]);
+
+      queryClient.setQueryData(['events', { id: id }], newEvent);
+
+      return { previousEvent };
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(['events', { id: id }], context.previousEvent);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(['events', { id: id }]);
+    },
   });
 
   function handleSubmit(formData) {
